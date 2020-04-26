df5a1534f8c565f323cb3daac68e6fda4fb5a7d0
frmdstryr <frmdstryr@protonmail.com>
2020-04-25 22:50:16 -0400

    Use python-clang package and add config_path arg

--- binder/common.py
+++ binder/common.py
@@ -17,9 +17,6 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
-import os
-import json
-
 SRC_PREFIX = """/*
 This file is part of pyOCCT which provides Python bindings to the OpenCASCADE
 geometry kernel.
